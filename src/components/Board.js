2e8dde69d3808072535c969f0b00bad1e9c256c6
Steven Bryant <stevelad_83@yahoo.com>
2023-01-26 15:32:06 -0500

    mapped through data and rendered a box for eaxch object

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Box from './Box.js';
+import boardData from '../board-data.js';
 
 export default function Board() {
-  return <Box />;
+  return (
+    <div className="board">
+      {boardData.map((box) => (
+        <Box key={box.space} />
+      ))}
+    </div>
+  );
 }
