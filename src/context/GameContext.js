8e251c5227c6762bc1e190483c730c244e0b5c16
Steven Bryant <stevelad_83@yahoo.com>
2023-01-27 17:36:17 -0500

    added checkMatch function

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -44,10 +44,20 @@ const useGameContext = () => {
   return context;
 };
 
+const checkMatch = (a, b, c) => {
+  if (!a || !b || !c) return false;
+  if (a === b && b === c) return true;
+};
+
 const checkWinner = (board) => {
-  if (board[0].content === board[1].content && board[1].content === board[2].content) {
-    console.log('winner');
-  }
+  if (checkMatch(board[0].content, board[1].content, board[2].content)) console.log('winner');
+  if (checkMatch(board[3].content, board[4].content, board[5].content)) console.log('winner');
+  if (checkMatch(board[6].content, board[7].content, board[8].content)) console.log('winner');
+  if (checkMatch(board[0].content, board[3].content, board[6].content)) console.log('winner');
+  if (checkMatch(board[1].content, board[4].content, board[7].content)) console.log('winner');
+  if (checkMatch(board[2].content, board[5].content, board[8].content)) console.log('winner');
+  if (checkMatch(board[0].content, board[4].content, board[8].content)) console.log('winner');
+  if (checkMatch(board[2].content, board[4].content, board[6].content)) console.log('winner');
 };
 
 export { GameProvider, useGameContext };
